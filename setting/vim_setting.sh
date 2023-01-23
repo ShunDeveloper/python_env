31d2fbc3bd861dd631ee75c22a847f8aa80ce072
ShunDeveloper <s.tsuchiya.business@gmail.com>
2023-01-23 17:31:50 +0000

    change setting files

--- setting/vim_setting.sh
+++ setting/vim_setting.sh
@@ -1,10 +1,14 @@
-# install latest vim
-apt-get install software-properties-common
-apt-get update
-add-apt-repository ppa:jonathonf/vim
-apt update
-apt install vim
+# if you not have vim: install latest vim
+#apt-get install software-properties-common
+#apt-get update
+#add-apt-repository ppa:jonathonf/vim
+#apt update
+#apt install vim
 vim --version
+
+# install monokai theme
+git clone git@github.com:sickill/vim-monokai.git
+cp vim-monokai/colors/monokai.vim ~/.vim/colors/
 
 # copy .vimrc
 cp vimrc ~/.vim/
