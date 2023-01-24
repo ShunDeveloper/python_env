34645575bf2eb3808b42ef8b3336a9ec10389488
ShunDeveloper <s.tsuchiya.business@gmail.com>
2023-01-24 12:52:08 +0000

    update readme and setting file for vim

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # EASY PYTHON ENV
+
+# GOAL
+
+### Infrastructure
+
+- create environment which is easy to build
+- Working on Ubuntu 20.04 and 22.04
+
+### setting file
+
+- `vim_setup.sh` and `vimrc` are file for create useful vim
+- `python_setup.sh` install `pyenv` and `poetry` to make virtual environment
+
+# Release
+
+- under developing `vim_setup.sh`
--- setting/vim_setting.sh
+++ setting/vim_setting.sh
@@ -1,10 +1,13 @@
-# if you not have vim: install latest vim
+
 #apt-get install software-properties-common
 #apt-get update
 #add-apt-repository ppa:jonathonf/vim
 #apt update
 #apt install vim
 vim --version
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 # install monokai theme
 git clone git@github.com:sickill/vim-monokai.git
